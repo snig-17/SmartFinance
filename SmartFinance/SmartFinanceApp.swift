eba6900c8ed69fd813835743c8e19a02c23091a8
snig-17 <snigdha.17.tiwari@icloud.com>
2025-08-08 12:11:01 +0530

    Initial Commit

---
+++ SmartFinance/SmartFinanceApp.swift
@@ -0,0 +1,20 @@
+//
+//  SmartFinanceApp.swift
+//  SmartFinance
+//
+//  Created by Snigdha Tiwari  on 08/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct SmartFinanceApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
